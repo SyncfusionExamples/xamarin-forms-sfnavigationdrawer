19f2cbc4b00fc4601be7c6c15d5297fdfa9153e9
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 19:38:24 +0530

    Getting started sample for navigation drawer

---
+++ GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hamburgerButton.Image = (FileImageSource)ImageSource.FromFile("hamburgericon.png");
+            List<string> list = new List<string>();
+            list.Add("Home");
+            list.Add("Profile");
+            list.Add("Inbox");
+            list.Add("Outbox");
+            list.Add("Sent");
+            list.Add("Draft");
+            listView.ItemsSource = list;
+        }
+
+        void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // Your codes here
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
